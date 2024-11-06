a9ded1f0ce4816f3a4621ce3ca8fc832b1c6c8e1
MisterMaker71 <Mister.Maker_71@gmx.de>
2024-11-06 11:50:29 +0100

    finished automation bat

--- Git.bat
+++ Git.bat
@@ -1,11 +1,15 @@
 @echo off
 git.exe pull %2
 
+pause
+
 git.exe add * %2
 
 set /p location="commit message:"
 
 git.exe commit -m "%location%" %2
 
+git.exe push %2
+echo saved changes
 pause
 EXIT
--- test.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cls
-
-set /p location="commit message:"
-echo We're working with %location%
-pause
-exit
