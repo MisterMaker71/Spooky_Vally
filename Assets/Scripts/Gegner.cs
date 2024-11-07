210fb10e10e577b344fef876403a3f96bae541ea
Ludcy <kurzawskikiko@gmail.com>
2024-11-07 15:16:04 +0100

    gegner kann nach zeit und range

--- Assets/Scripts/Gegner.cs
+++ Assets/Scripts/Gegner.cs
@@ -6,25 +6,40 @@ public class Gegner : MonoBehaviour
 {
     public Transform player;
     public float speed = 2.0f;
-
-    public float followRange = 10.0f;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float followRange = 20.0f;
+    public int health= 100;
+    private float timeSinceoutofrange = 0.0f;
+    public float maxOutofrangetime = 10.0f;
+    
+    //takedmg 
+    //eigenes script für den spawner
+    //spawner als likste 3gegener 
 
     // Update is called once per frame
     void Update()
     {
-    if (player != null)
-    {
-        if(Vector3.Distance(transform.position,player.position) <followRange)
+        if (player != null)
         {
-            transform.position = Vector3.MoveTowards(transform.position, player.position , Time.deltaTime * speed);
+            float distanceToplayer = Vector3.Distance(transform.position, player.position);
+            if (distanceToplayer < followRange)
+        {
+            timeSinceoutofrange = 0.0f;
+            transform.position = Vector3.MoveTowards(transform.position, player.position, Time.deltaTime * speed);
+        }
+        else
+        {
+            timeSinceoutofrange += Time.deltaTime;
         }
     }
+
+        if (timeSinceoutofrange >= maxOutofrangetime)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
     }
 }
